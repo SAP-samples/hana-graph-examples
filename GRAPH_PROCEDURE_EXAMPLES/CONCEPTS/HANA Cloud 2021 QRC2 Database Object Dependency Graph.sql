aff4da90ecf884a3af51ace1c5ce4703dc3de39a
Markus Fath <markus.fath@sap.com>
2021-07-28 08:36:20 +0200

    Update HANA Cloud 2021 QRC2 Database Object Dependency Graph.sql

--- GRAPH_PROCEDURE_EXAMPLES/CONCEPTS/HANA Cloud 2021 QRC2 Database Object Dependency Graph.sql
+++ GRAPH_PROCEDURE_EXAMPLES/CONCEPTS/HANA Cloud 2021 QRC2 Database Object Dependency Graph.sql
@@ -8,6 +8,7 @@
 -- On the graph workspace, we can use graph functions and algorithms.
 -- For getting object dependencies plus hop distance, we use the built-in algorithm "shortest paths one to all".
 
+DROP SCHEMA "DEP_ANALYSIS_HANA" CASCADE;
 CREATE SCHEMA "DEP_ANALYSIS_HANA";
 -- The edges of the object dependency graph are in the OBJECT_DEPENDENCIES system view
 CREATE OR REPLACE VIEW "DEP_ANALYSIS_HANA"."V_EDGES" AS (
@@ -27,31 +28,70 @@ CREATE OR REPLACE VIEW "DEP_ANALYSIS_HANA"."V_VERTICES" AS (
 	SELECT * FROM "SYS"."OBJECTS" WHERE "OBJECT_OID" IN (
 		SELECT "SOURCE" FROM "DEP_ANALYSIS_HANA"."V_EDGES") OR "OBJECT_OID" IN (SELECT "TARGET" FROM "DEP_ANALYSIS_HANA"."V_EDGES")
 );
+-- Optionally: persist the data
+CREATE COLUMN TABLE "DEP_ANALYSIS_HANA"."T_VERTICES" AS (SELECT * FROM "DEP_ANALYSIS_HANA"."V_VERTICES");
+ALTER TABLE "DEP_ANALYSIS_HANA"."T_VERTICES" ADD PRIMARY KEY ("OBJECT_OID");
+CREATE COLUMN TABLE "DEP_ANALYSIS_HANA"."T_EDGES" AS (SELECT * FROM "DEP_ANALYSIS_HANA"."V_EDGES");
+ALTER TABLE "DEP_ANALYSIS_HANA"."T_EDGES" ADD PRIMARY KEY ("KEY");
+ALTER TABLE "DEP_ANALYSIS_HANA"."T_EDGES" ALTER ("SOURCE" BIGINT NOT NULL);
+ALTER TABLE "DEP_ANALYSIS_HANA"."T_EDGES" ALTER ("TARGET" BIGINT NOT NULL);
+
 -- Graph Workspace
-CREATE GRAPH WORKSPACE "DEP_ANALYSIS_HANA"."V_OBJECT_GRAPH"
-	EDGE TABLE "DEP_ANALYSIS_HANA"."V_EDGES"
+CREATE GRAPH WORKSPACE "DEP_ANALYSIS_HANA"."T_OBJECT_GRAPH"
+	EDGE TABLE "DEP_ANALYSIS_HANA"."T_EDGES"
 		SOURCE COLUMN "SOURCE"
 		TARGET COLUMN "TARGET"
 		KEY COLUMN "KEY"
-	VERTEX TABLE "DEP_ANALYSIS_HANA"."V_VERTICES" 
+	VERTEX TABLE "DEP_ANALYSIS_HANA"."T_VERTICES" 
 		KEY COLUMN "OBJECT_OID";
 
 
 /****************************************/
 -- We use shortest paths one to all (SPOA) to identify all dependent objects in one traversal direction (INCOMING or OUTGOING), and to get the spanning tree
 /****************************************/
+-- get the OBJECT_OID of one of your vertices 
+SELECT * FROM "DEP_ANALYSIS_HANA"."V_VERTICES";
+-- the logic runs as "anonymous block" (SAP HANA Coud only)
 DO(
-	IN i_start BIGINT => 201917,--201935,	-- the key of the start vertex
-	IN i_dir NVARCHAR(10) => 'INCOMING', 	-- the the direction of the edge traversal: OUTGOING (default), INCOMING
-	OUT o_vertices TABLE ("OBJECT_OID" BIGINT, "SCHEMA_NAME" NVARCHAR(5000), "OBJECT_NAME" NVARCHAR(5000), "OBJECT_TYPE"  NVARCHAR(5000), "DISTANCE" BIGINT) => ?,
-	OUT o_edges TABLE ("KEY" NVARCHAR(100), "SOURCE" BIGINT, "TARGET" BIGINT, "DEPENDENCY_TYPE" INTEGER, "DEPENDENCY_LABEL" NVARCHAR(10)) => ?
+	IN i_start BIGINT => [put an OBJECT_OID here],			-- the key of the start vertex
+	IN i_dir VARCHAR(10) => 'INCOMING', 	-- the the direction of the edge traversal: OUTGOING (default), INCOMING
+	OUT o_vertices TABLE ("OBJECT_OID" BIGINT, "SCHEMA_NAME" NVARCHAR(5000), "OBJECT_NAME" NVARCHAR(5000), "OBJECT_TYPE" VARCHAR(5000), "DISTANCE" BIGINT) => ?,
+	OUT o_edges TABLE ("KEY" VARCHAR(100), "SOURCE" BIGINT, "TARGET" BIGINT, "DEPENDENCY_TYPE" INTEGER, "DEPENDENCY_LABEL" VARCHAR(10)) => ?
 	)
 LANGUAGE GRAPH
 BEGIN
-	GRAPH g = Graph("DEP_ANALYSIS_HANA","V_OBJECT_GRAPH");
+	GRAPH g = Graph("DEP_ANALYSIS_HANA","T_OBJECT_GRAPH");
 	VERTEX v_start = Vertex(:g, :i_start);
 	GRAPH g_spoa = SHORTEST_PATHS_ONE_TO_ALL(:g, :v_start, "DISTANCE", :i_dir);
 	o_vertices = SELECT :v."OBJECT_OID", :v."SCHEMA_NAME", :v."OBJECT_NAME", :v."OBJECT_TYPE", :v."DISTANCE" FOREACH v IN Vertices(:g_spoa);
 	o_edges = SELECT :e."KEY", :e."SOURCE", :e."TARGET", :e."DEPENDENCY_TYPE", :e."DEPENDENCY_LABEL" FOREACH e IN Edges(:g_spoa);
 END;
 
+
+
+-- the same logic runs as procedure (SAP HANA Cloud and on-premise)
+-- create table types
+CREATE TYPE "DEP_ANALYSIS_HANA"."TT_EDGES" AS TABLE ("KEY" VARCHAR(100), "SOURCE" BIGINT, "TARGET" BIGINT, "DEPENDENCY_TYPE" INTEGER, "DEPENDENCY_LABEL" VARCHAR(10));
+CREATE TYPE "DEP_ANALYSIS_HANA"."TT_VERTICES" AS TABLE ("OBJECT_OID" BIGINT, "SCHEMA_NAME" NVARCHAR(5000), "OBJECT_NAME" NVARCHAR(5000), "OBJECT_TYPE"  VARCHAR(5000), "DISTANCE" BIGINT);
+
+CREATE OR REPLACE PROCEDURE "DEP_ANALYSIS_HANA"."GS_SPOA" (
+	IN i_start BIGINT,		-- the key of the start vertex
+	IN i_dir VARCHAR(10), 	-- the the direction of the edge traversal: OUTGOING (default), INCOMING
+	OUT o_vertices "DEP_ANALYSIS_HANA"."TT_VERTICES",
+	OUT o_edges "DEP_ANALYSIS_HANA"."TT_EDGES"
+	)
+LANGUAGE GRAPH READS SQL DATA AS
+BEGIN
+	GRAPH g = Graph("DEP_ANALYSIS_HANA","T_OBJECT_GRAPH");
+	VERTEX v_start = Vertex(:g, :i_start);
+	GRAPH g_spoa = SHORTEST_PATHS_ONE_TO_ALL(:g, :v_start, "DISTANCE", :i_dir);
+	o_vertices = SELECT :v."OBJECT_OID", :v."SCHEMA_NAME", :v."OBJECT_NAME", :v."OBJECT_TYPE", :v."DISTANCE" FOREACH v IN Vertices(:g_spoa);
+	o_edges = SELECT :e."KEY", :e."SOURCE", :e."TARGET", :e."DEPENDENCY_TYPE", :e."DEPENDENCY_LABEL" FOREACH e IN Edges(:g_spoa);
+END;
+
+-- get the OBJECT_OID of one of your vertices 
+SELECT * FROM "DEP_ANALYSIS_HANA"."V_VERTICES";
+
+-- call the procedure
+CALL "DEP_ANALYSIS_HANA"."GS_SPOA"([put an OBJECT_OID here], 'OUTGOING', ?, ?);
+CALL "DEP_ANALYSIS_HANA"."GS_SPOA"([put an OBJECT_OID here], 'INCOMING', ?, ?);
